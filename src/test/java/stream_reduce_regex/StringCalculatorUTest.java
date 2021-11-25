fe98b07c01195b34e4bfa6c25806b5daaf4b922d
Thibaut Cantet <thibaut.cantet@soat.fr>
2021-11-25 09:40:01 +0100

    keep first unit test

--- src/test/java/stream_reduce_regex/StringCalculatorUTest.java
+++ src/test/java/stream_reduce_regex/StringCalculatorUTest.java
@@ -13,31 +13,4 @@ class StringCalculatorUTest {
 
         assertThat(result).isEqualTo(444);
     }
-
-    @Test
-    void add_should_sum_all_numbers_except_not_valid_number(){
-        final String input = "1,5,8,,98,fg,178,154";
-
-        int result = new StringCalculator().calculate(input);
-
-        assertThat(result).isEqualTo(444);
-    }
-
-    @Test
-    void calculate_should_sum_all_int_when_delimiter_is_defined() {
-        final String input = "\\[delime]\n1delime5delime8delime98delime178delime 154";
-
-        int result = new StringCalculator().calculate(input);
-
-        assertThat(result).isEqualTo(444);
-    }
-
-    @Test
-    void add_should_sum_all_numbers_when_delimiter_is_defined_and_is_pipe(){
-        String input = "\\[|]\n1|5|8|98|178|154";
-
-        int result = new StringCalculator().calculate(input);
-
-        assertThat(result).isEqualTo(444);
-    }
 }
