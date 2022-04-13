495e80bab3ff4ef43347ef1fed35fb5574b782ec
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-04-13 16:50:41 +0200

    keep first unit test

--- src/test/java/stream_reduce_regex/StringCalculatorUTest.java
+++ src/test/java/stream_reduce_regex/StringCalculatorUTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringCalculatorUTest {
+
+    StringCalculator stringCalculator = new StringCalculator();
+
     @Test
     void calculate_should_sum_all_int() {
-        final String input = "1,5,8,98,178, 154";
+        final String input = "1,5,8,98,g,178, 154";
 
-        int result = new StringCalculator().calculate(input);
+        int result = stringCalculator.calculate(input);
 
         assertThat(result).isEqualTo(444);
     }
